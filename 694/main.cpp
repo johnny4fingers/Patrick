4a2535e2dee68133608a611fa078a45f7f8ac2b0
Patrick0508 <100470538+Patrick0508@users.noreply.github.com>
2022-03-23 17:44:54 +0200

    http://info.mcip.ro/?cap=Tablouri%20bidimensionale&prob=694

---
+++ 694/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+/*Se citeste un numar natural n (n<=20). Construiti si afisati o matrice patratica cu n linii si n coloane care sa contina
+primii n*n multipli ai lui n aranjati ca �n exemplul de mai jos.
+Exemplu: Pentru n=4 se obtine matricea:
+4 20 36 52
+8 24 40 56
+12 28 44 60
+16 32 48 64
+*/
+
+
+int main()
+{
+
+    return 0;
+}
