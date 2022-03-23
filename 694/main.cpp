9605068c44ed69d3e4971456c22d701a77b4c979
Patrick0508 <100470538+Patrick0508@users.noreply.github.com>
2022-03-23 17:20:54 +0200

    tema

---
+++ 694/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+/*Se citeste un numar natural n (n<=20). Construiti si afisati o matrice patratica cu n linii si n coloane care sa contina
+primii n*n multipli ai lui n aranjati ca �n exemplul de mai jos.
+*/
+
+int main()
+{
+
+    return 0;
+}
