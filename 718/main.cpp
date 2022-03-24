7b648047be74d1d70463294f14340d779475297f
Patrick0508 <100470538+Patrick0508@users.noreply.github.com>
2022-03-24 17:10:01 +0200

    718

---
+++ 718/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+/*#718.
+Se citeste un numar natural n (n<=100). Construiti si afisati o matrice patratica cu n linii si n coloane dupa modelul de mai jos, obtinut pentru n=9.
+5 4 3 2 1 2 3 4 5
+4 5 4 3 2 3 4 5 4
+3 4 5 4 3 4 5 4 3
+2 3 4 5 4 5 4 3 2
+1 2 3 4 5 4 3 2 1
+2 3 4 5 4 5 4 3 2
+3 4 5 4 3 4 5 4 3
+4 5 4 3 2 3 4 5 4
+5 4 3 2 1 2 3 4 5
+*/
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
