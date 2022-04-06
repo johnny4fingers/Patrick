1fd8b4ddfb17e50422951fff35e75612a6016c67
Patrick0508 <100470538+Patrick0508@users.noreply.github.com>
2022-04-06 20:07:39 +0300

    456

---
+++ 456/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+
+#456.
+Se citeste o matrice cu n linii si m coloane avand elementele 0 sau 1. Afisati perechile de linii complementare (suma elementelor de pe cele doua linii si de pe aceleasi coloane sa fie 1).
+Exemplu:
+n=5 m=3
+matricea
+1 0 1
+1 1 1
+0 1 0
+0 0 0
+1 1 0
+are perechile de linii complementare (1,3) si (2,4)
+Liniile 1 si 3 sunt complementare deoarece
+1 0 1 +
+0 1 0
+_____
+1 1 1
+*/
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
