aeebabd00b65d32f65fa60f298d13a3b6c7ccda7
Pop Adrian <popsadrian@gmail.com>
2022-04-18 17:10:57 +0300

    Update main.cpp

--- 771 pbinfo/main.cpp
+++ 771 pbinfo/main.cpp
@@ -17,19 +17,51 @@ date de iesire:
 4 20 15 23 18 9
 17 15 13 18 12 15
 */
+// declarate aici ca sa avem vizibilitate in toate functiile nu doar in main
+int arr[101][101], m;
+
+// functie care efectueaza interschimbarea intre doua linii
+void interschimbare(int i, int j) {
+    for (int k = 0; k < m; k++) {
+        int aux = arr[i][k];
+        arr[i][k] = arr[j][k];
+        arr[j][k] = aux;
+    }
+}
+
+// functie care primeste parametru indexul liniei "i" si returneaza suma elementelor liniei
+int suma(int i) {
+    int sumaLiniei = 0;
+    for (int j = 0; j < m; j++) {
+        sumaLiniei = sumaLiniei + arr[i][j];
+    }
+
+    return sumaLiniei;
+}
 
 int main()
 {
-    int n, m, a[101][101], i, j, ok=0, aux, k, q, b, r;
-
+    // int n, m, a[101][101], i, j, ok=0, aux, k, q, b, r;
+    int n;
     cin >> n >> m;
 
-    for(i = 1; i <= n; i++){
-        for(j = 1; j <= m; j++){
-            cin >> a[i][j];
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            cin >> arr[i][j];
         }
     }
 
+    // bubble-sort pentru linii
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if(suma(i) > suma(j)) {
+                // interschimbam linia i cu linia j
+                interschimbare(i, j);
+            }
+        }
+    }
+
+    /*
     while(ok == 0){
         ok = 1;
         for(i = 1, j = i + 1; i < n, j <= n; i++, j++){
@@ -48,12 +80,12 @@ int main()
                 }
             }
         }
-    }
+    }*/
 
 
-    for(i = 1; i <= n; i++){
-        for(j = 1; j <= m; j++){
-            cout << a[i][j] << " ";
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            cout << arr[i][j] << " ";
         }
         cout << endl;
     }
