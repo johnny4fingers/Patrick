8b80443f52aef38324389746536ed79b2c6bf997
Pop Adrian <popsadrian@gmail.com>
2022-04-18 15:28:51 +0300

    Update main.cpp

--- 768 pbinfo/main.cpp
+++ 768 pbinfo/main.cpp
@@ -19,10 +19,52 @@ date de iesire:
 
 int main()
 {
-    int n, m, a[101][101], p[10201], k = 1, ok = 1, aux, s = 0;
+    // foarte complicat
+    // la citire poti verifica paritatea si distinctia
+
+    // suma pe care o vomavea de afisat
+    int suma = 0;
+    // array unde salvam/verificam ce am adaugat la suma
+    int arr[100];
+    // counter sa stim cate elemente avem in arr[]
+    int counter = 0;
+    //
+    int n, m;
+
+    // int n, m, a[101][101], p[10201], k = 1, ok = 1, aux, s = 0;
 
     cin >> n >> m;
 
+    // citire
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            // definim o variabila int sa putem stoca valoarea introdusa
+            int nr;
+            cin >> nr;
+            // vedem daca ii par
+            if (nr % 2 == 0) {
+                // verificam daca se afla in array
+                bool gasit = false;
+                for(int k = 0; k < counter; k++) {
+                    if (arr[k] == nr) {
+                        gasit = true;
+                    }
+                }
+                // in cazul in care nu am gasit numarul introdus in array trebuie introdus si adugat la suma
+                if (gasit == false) {
+                    suma = suma + nr;
+                    // incrementam counterul ca sa stim cate elemente avem in array si pe ce loc punem urmatorul numar
+                    counter++;
+                    // adaugam pe
+                    arr[counter] = nr;
+                }
+            }
+        }
+    }
+
+    // afisare
+    cout << suma;
+/*
     for(int i = 1; i <= n; i++){
         for(int j = 1; j <= m; j++){
             cin >> a[i][j];
@@ -53,6 +95,6 @@ int main()
     }
 
     cout << s;
-
+*/
     return 0;
 }
